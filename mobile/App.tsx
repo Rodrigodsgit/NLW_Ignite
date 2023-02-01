ca96e0b0a440ea11c0ee507d3cf8afa914e76995
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2023-01-31 23:00:03 -0300

    Add: tabelas do bd (Participant, User, Game,Guess)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar} from "native-base";
 import  {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
 import { Loading } from './components/Loading';
-import { SignIn } from './screens/Signin';
+import { SignIn } from './screens/SignIn';
 
 
 import {THEME} from './src/styles/theme';
